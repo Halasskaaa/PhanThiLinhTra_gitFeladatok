e60bc6afcfc72e209e06a95d44f1a61b4eef900c
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2024-12-19 17:11:33 +0100

    F01 feladat elkészítve

--- git/Program.cs
+++ git/Program.cs
@@ -8,6 +8,14 @@ namespace git
 {
     internal class Program
     {
+        static Random rnd = new Random();
+        public static void F01(int[] szamok)
+        {
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                szamok[i] = rnd.Next(1,51);
+            }
+        }
         static void Main(string[] args)
         {
             
